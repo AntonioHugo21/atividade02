ca39d3d9eaeaed6b44bfffc9c9500d8b65b31762
AntonioHugo21 <antoniohugo.pierre@gmail.com>
2024-09-04 15:26:52 -0300

    convesão de graus

---
+++ atividade02.py
@@ -0,0 +1,6 @@
+#Crie um programa que converta uma temperatura dada em Celsius para Fahrenheit usando a fórmula: F = C * 9/5 + 32.
+c = int(input("digite a temperatura"))
+
+f = c * 9/5 + 32
+
+print(f"a temperatura {c} em graus convertida para fahrenheit é : {f}")
